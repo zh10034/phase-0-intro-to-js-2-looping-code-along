78b3a335f2b1a3757a67d2b0b618ee45099d1f47
zh10034 <zh10034@LAPTOP-KH5314B2.localdomain>
2023-08-16 17:18:16 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const newArray= [];
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+     return newArray;
+}
+
+function countDown() {
+    let i = 10;
+    while (i > -1) {
+        console.log(i--);
+    }
+};
